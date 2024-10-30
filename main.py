363a7c855ca12241b0797fd6a13de66d98bc06e7
Park JeongSeon <105702539+j28s@users.noreply.github.com>
2024-10-30 17:15:09 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from github_utils import get_github_repo, upload_github_issue
 if __name__ == "__main__":
     # access_token = os.environ['MY_GITHUB_TOKEN']
     # access_token = os.environ.get('MY_GITHUB_TOKEN', None)
-    # access_token = os.getenv('MY_GITHUB_TOKEN')
-    # if not access_token:
-    #     print("Error: MY_GITHUB_TOKEN is not set.")
+    access_token = os.getenv('MY_GITHUB_TOKEN')
+    if not access_token:
+        print("Error: MY_GITHUB_TOKEN is not set.")
     repository_name = "SAP_2024_08"
 
     seoul_timezone = timezone('Asia/Seoul')
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     upload_contents = "\n\n".join(
         [f"### {article['title']} ({article['date']})\n- URL: {article['url']}\n- 내용: {article['content']}" for article in articles]
     )
-    
+
     # GitHub 업로드 대신 콘솔에 출력
     print("Issue Title:", issue_title)
     print("Upload Contents:")
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     print("Upload Github Issue Success!")  # 실제로 업로드하지 않고 성공 메시지 출력
 
     # GitHub에 Issue 업로드
-    repo = get_github_repo(repository_name)
+    repo = get_github_repo(access_token, repository_name)
     # repo = get_github_repo(repository_name)
     upload_github_issue(repo, issue_title, upload_contents)
     print("Upload Github Issue Success!")
